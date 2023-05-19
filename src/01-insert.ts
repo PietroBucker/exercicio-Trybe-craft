e8f530e62f9b9a24e14ae43042261e6845eb8c92
Pietro Bucker <pietro_177@hotmail.com>
2023-05-19 19:59:22 -0300

    req 1 2 e 3

--- src/01-insert.ts
+++ src/01-insert.ts
@@ -1,3 +1,3 @@
-// export default function insert() {
-//   // insira a solução
-// }
+export default function insert(product: string):string {
+  return `${product} adicionado(a) com sucesso!`
+}
--- src/02-insertProducts.ts
+++ src/02-insertProducts.ts
@@ -1,3 +1,19 @@
-// export default function insertProducts() {
-//   // insira a solução
-// }
+export type Product = {
+  id: number,
+  name: string,
+  price: number,
+  quantity: number,
+  brands: string[],
+};
+
+export default function insertProducts(brands: string[], product: Product): string {
+  console.log(brands);
+  console.log(product.brands);
+  
+  const brandExist: boolean = brands.every((element) => product.brands.includes(element));
+  
+  if (brandExist) {
+    return `${product.name} adicionado(a) com o preço de R$ ${product.price}`;
+  }
+  return 'Seu produto contém marcas indisponíveis.';
+}
