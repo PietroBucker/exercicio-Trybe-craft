44f07ec4c8f8005628f367624e596eb89493bb34
Pietro Bucker <pietro_177@hotmail.com>
2023-05-21 20:19:23 -0300

    req 4 e 5 rquisito 5 nao estava passando por uma maldito PONTO

--- src/04-calculateTotalPrice.ts
+++ src/04-calculateTotalPrice.ts
@@ -1,3 +1,14 @@
-// export default function calculateTotalPrice() {
-//   // insira a solução
-// }
+import { Product } from './02-insertProducts';
+
+export type Item = {
+  product: Product
+  quantity: number,
+  phrase: string,
+  brand: string,
+};
+
+export default function calculateTotalPrice(itens: Item[]):number | [] {
+  const result = itens.reduce((acc, item) => acc + (item.product.price * item.quantity), 0);
+  
+  return parseFloat(result.toFixed(2));
+}
--- src/05-orderRequest.ts
+++ src/05-orderRequest.ts
@@ -1,3 +1,21 @@
-// export default function orderRequest() {
-//   // insira a solução
-// }
+import { Item } from './04-calculateTotalPrice';
+
+export type Status = 'pendente' | 'enviado' | 'entregue';
+
+export type Order = {
+  id: number,
+  customerName: string,
+  customerEmail: string,
+  items: Item[]
+  status: Status,
+};
+
+export default function orderRequest(order: Order):string {
+  console.log(order);
+  
+  const outStockItem = order.items.find((item) => item.product.quantity === 0);
+  if (outStockItem) {
+    throw new Error(`Desculpe, ${outStockItem.product.name} não está disponível no estoque`);
+  }
+  return `Olá ${order.customerName}, o seu pedido de ID ${order.id} foi enviado.`;
+}
